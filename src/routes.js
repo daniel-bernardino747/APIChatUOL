7c1cd0f48e06632d4e727a461ed059a3583b0c97
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-09 14:50:35 -0300

    feat: routes participants

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import { returnParticipants, registerParticipant } from './Controllers/Participants.js';
 
 const routes = express.Router();
+
+routes.get('/participants', returnParticipants);
+
+routes.post('/participants', registerParticipant);
 
 export default routes;
