11357674ad1abeb6b8e7250599870ec9fc5a980e
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-11 14:34:00 -0300

    style: exchange positions routes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,19 @@
 import express from 'express';
+
+import updateStatus from './Controllers/StatusController.js';
+
 import { returnParticipants, registerParticipant } from './Controllers/ParticipantsController.js';
 import { returnMessages, sendMessage } from './Controllers/MessagesController.js';
-import updateStatus from './Controllers/StatusController.js';
 
 const routes = express.Router();
 
-routes.get('/participants', returnParticipants);
-
 routes.post('/participants', registerParticipant);
 
-routes.get('/messages', returnMessages);
+routes.get('/participants', returnParticipants);
 
 routes.post('/messages', sendMessage);
+
+routes.get('/messages', returnMessages);
 
 routes.post('/status', updateStatus);
 
